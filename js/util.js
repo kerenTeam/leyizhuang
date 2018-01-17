14e1946bb1c8c163853690d4c5fe94ce026a1a14
thefinccc <1425302858@qq.com>
2018-01-17 19:01:34 +0800

    no message

--- js/util.js
+++ js/util.js
@@ -77,6 +77,9 @@ var util = {
 	 * 点击重绘底部tab （view控件）
 	 */
 	toggleNview: function(currIndex) {
+		if(currIndex !=2){ 
+			mui.fire(plus.webview.getWebviewById('cart.html'),'refreshBtn')
+		}
 		currIndex = currIndex * 2;
 		// 重绘当前tag 包括icon和text，所以执行两个重绘操作
 		util.updateSubNView(currIndex, util.options.ACTIVE_COLOR);
